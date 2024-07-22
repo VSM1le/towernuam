78f0bad2f08fbfe1cc43cae58130cb62b9ba43b3
earth <earthfg@gmail.com>
2024-07-22 20:07:07 +0700

    rent 2

--- app/Livewire/Cusrent.php
+++ app/Livewire/Cusrent.php
@@ -26,6 +26,10 @@ class Cusrent extends Component
     public $tower = "A";
 
     public $showCreateContract = false;
+
+    public $showEditContract = false;
+    public $editId;
+
      #[Computed()]
     public function customers(){
         return Customer::all();
@@ -53,6 +57,49 @@ class Cusrent extends Component
     }
     public function closeCreateContract(){
         $this->showCreateContract = false;
+        $this->reset(['contractNumber','contractrNumber','customerId','unit','areaSqm','rentalFee','serviceFee','equipFee','startDate','endDate','year']);
+    }
+
+    public function openEditContract($id){
+     
+        $this->editId = $id;
+        $contract = CustomerRental::where('id',$id)->get();
+        $this->customerId = $contract->customer_id;
+        $this->contractNumber = $contract->custr_contract_no;
+        $this->contractRNumber = $contract->custr_contract_no_real; 
+        $this->unit = $contract->custr_unit;
+        $this->areaSqm = $contract->custr_area_sqm;
+        $this->rentalFee = $contract->custr_service_fee;
+        $this->serviceFee = $contract->custr_service_fee;
+        $this->equipFee = $contract->custr_equipment_fee;
+        $this->startDate = $contract->custr_begin_date2;
+        $this->endDate = $contract->custr_end_date2;
+    
+        $this->showEditContract = true;
+    } 
+
+    public function editContract(){
+        CustomerRental::where('id',$this->editId)->update([
+            'customer_id' => $this->customerId,
+            'custr_no' => 1,
+            'custr_contract_no' => $this->contractNumber,
+            'custr_contract_no_real' => $this->contractRNumber,
+            'custr_tower' => $this->tower,
+            'custr_unit' => $this->unit,
+            'custr_area_sqm' => $this->areaSqm,
+            'custr_rental_fee' => $this->rentalFee,
+            'custr_service_fee' => $this->serviceFee,
+            'custr_equipment_fee' => $this->equipFee,
+            'custr_begin_date2' => $this->startDate,
+            'custr_end_date2' => $this->endDate,
+            'created_by' => auth()->id(),
+            'updated_by' => auth()->id(),
+        ]);
+    }
+
+    public function closeEditContract(){
+        $this->reset(['contractNumber','contractrNumber','customerId','unit','areaSqm','rentalFee','serviceFee','equipFee','startDate','endDate','year','editId']);
+        $this->showEditContract = false;
     }
 
     public function render()
--- resources/views/livewire/cusrent.blade.php
+++ resources/views/livewire/cusrent.blade.php
@@ -79,7 +79,106 @@
     </div>
 
 
- @if($showCreateContract)
+ @if($showEditContract)
+    <div class="fixed inset-0 bg-gray-300 opacity-40"  wire:click="closeEditContract"></div>
+    <form wire:submit.prevent="editContract" class="flex flex-col justify-between bg-white rounded m-auto fixed inset-0" 
+     :style="{ 'max-height': '550px', 'max-width' : '600px' }">
+        <div class="bg-blue-700 text-white w-full px-4 py-3 flex items-center justify-between border-b border-gray-300">
+            <div class="text-xl font-bold">Create Contract</div>
+            <button wire:click="closeEditContract" type="button" class="focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+        <div class="flex-grow bg-white w-full flex flex-col items-center justify-start overflow-y-auto">
+            <div class="flex justify-start w-full">
+                <div class="m-3 ml-5">
+                    <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Customer</label>
+                        <label class="w-52 text-sm font-medium text-gray-900"></label>
+                        <select wire:model="customerId" required
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option value="">select customer</option>
+                            @foreach ($this->customers as $customer )
+                               <option value="{{ $customer->id }}">{{ $customer->cust_code }} : {{ $customer->cust_name_th }}</option> 
+                            @endforeach 
+                        </select>
+                </div>
+                <div class="m-3 mr-5">
+                    <label 
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contract number</label>
+                    <input  wire:model="contractNumber"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+            </div>
+            <div class="flex justify-start w-full">
+               <div class="m-3 ml-5">
+                    <label 
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contract real number</label>
+                    <input  wire:model="contractRNumber"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div> 
+                <div class="m-3 ml-5">
+                    <label 
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">start date</label>
+                    <input  wire:model="startDate" type="date"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <div class="m-3 ml-5">
+                    <label 
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">end date</label>
+                    <input  wire:model="endDate" type="date"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+            </div>
+             <div class="flex justify-start w-full">
+                <div class="m-3 w-24 ml-5">
+                    <label 
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+                    <input  wire:model="year"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div> 
+                <div class="m-3 w-full mr-5">
+                    <label class="block mb-2 text-sm w-full font-medium text-gray-900 dark:text-white">Unit</label>
+                    <input wire:model="unit" 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+            </div>
+            <div class="flex justify-start">
+                <div class="m-3 w-full ml-5">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Area sqm</label>
+                    <input wire:model="areaSqm" step="0.01" type="number" 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                <div class="m-3 w-full">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rental fee</label>
+                    <input wire:model="rentalFee" step="0.01"  type="number"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                <div class="m-3 w-full ">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service fee</label>
+                    <input type="number"  wire:model="serviceFee" step="0.01" 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+                 <div class="m-3 w-full mr-5">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Equipment fee</label>
+                    <input type="number"  wire:model="equipFee" step="0.01" 
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
+            </div>
+        </div>
+        <div class="bg-gray-100 w-full flex justify-between p-4">
+            <div class="flex">
+            </div>
+            <div>
+            <button type="submit" 
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">Save</button>
+            </div>
+        </div>
+    </form>
+    </div>
+    @endif 
+    @if($showCreateContract)
     <div class="fixed inset-0 bg-gray-300 opacity-40"  wire:click="closeCreateContract"></div>
     <form wire:submit.prevent="createContract" class="flex flex-col justify-between bg-white rounded m-auto fixed inset-0" 
      :style="{ 'max-height': '550px', 'max-width' : '600px' }">
