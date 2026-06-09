e9793f3c433d99715cad3836214b28e15d207101
VSM1le <135961158+VSM1le@users.noreply.github.com>
2026-06-09 17:25:50 +0700

    Update invoicePeriod to return static date range
    Refactor invoicePeriod method to return a static date range instead of calculating it based on PsGroup parameters.

--- app/Services/periodPs.php
+++ app/Services/periodPs.php
@@ -6,40 +6,43 @@ use Carbon\Carbon;
 
 class periodPs{
 
+    // public function invoicePeriod($date ,PsGroup $psGroup){
+    //     if($psGroup->ps_period == "last"){
+    //         $carbonDate = Carbon::parse($date);
+    //         $start = $carbonDate->copy()->subMonth()->day($psGroup->begin_date)->format('d/m/Y');
+    //         if($psGroup->end_date == "31"){
+    //             $end = $carbonDate->copy()->endOfMonth()->format('d/m/Y');
+    //         }
+    //         else{
+    //             $end = $carbonDate->copy()->day($psGroup->end_date)->format('d/m/Y');
+    //         }
+    //         $period = $start . " - " . $end;
+    //     } 
+    //     elseif($psGroup->ps_period == "present"){
+    //         $carbonDate = Carbon::parse($date);
+    //         $start = $carbonDate->copy()->day($psGroup->begin_date)->format('d/m/Y');
+    //         if($psGroup->end_date == "31"){
+    //             $end = $carbonDate->copy()->endOfMonth()->format('d/m/Y');
+    //         }
+    //         else{
+    //             $end = $carbonDate->copy()->day($psGroup->end_date)->format('d/m/Y');
+    //         }
+    //         $period = $start . " - " . $end;
+    //     }
+    //     else{
+    //         $carbonDate = Carbon::parse($date);
+    //         $start = $carbonDate->copy()->addMonth()->day($psGroup->begin_date)->format('d/m/Y');
+    //         if($psGroup->end_date == "31"){
+    //             $end = $carbonDate->copy()->addMonth()->endOfMonth()->format('d/m/Y');
+    //         }
+    //         else{
+    //             $end = $carbonDate->copy()->addMonth()->day($psGroup->end_date)->format('d/m/Y');
+    //         }
+    //         $period = $start . " - " . $end;
+    //     }
+    //     return $period;
+    // }
     public function invoicePeriod($date ,PsGroup $psGroup){
-        if($psGroup->ps_period == "last"){
-            $carbonDate = Carbon::parse($date);
-            $start = $carbonDate->copy()->subMonth()->day($psGroup->begin_date)->format('d/m/Y');
-            if($psGroup->end_date == "31"){
-                $end = $carbonDate->copy()->endOfMonth()->format('d/m/Y');
-            }
-            else{
-                $end = $carbonDate->copy()->day($psGroup->end_date)->format('d/m/Y');
-            }
-            $period = $start . " - " . $end;
-        } 
-        elseif($psGroup->ps_period == "present"){
-            $carbonDate = Carbon::parse($date);
-            $start = $carbonDate->copy()->day($psGroup->begin_date)->format('d/m/Y');
-            if($psGroup->end_date == "31"){
-                $end = $carbonDate->copy()->endOfMonth()->format('d/m/Y');
-            }
-            else{
-                $end = $carbonDate->copy()->day($psGroup->end_date)->format('d/m/Y');
-            }
-            $period = $start . " - " . $end;
-        }
-        else{
-            $carbonDate = Carbon::parse($date);
-            $start = $carbonDate->copy()->addMonth()->day($psGroup->begin_date)->format('d/m/Y');
-            if($psGroup->end_date == "31"){
-                $end = $carbonDate->copy()->addMonth()->endOfMonth()->format('d/m/Y');
-            }
-            else{
-                $end = $carbonDate->copy()->addMonth()->day($psGroup->end_date)->format('d/m/Y');
-            }
-            $period = $start . " - " . $end;
-        }
-        return $period;
+        return "04/05/2026 - 31/05/2026";
     }
 } 
